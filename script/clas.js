3847597992377f3c853a6e75ad2b3ca62e1d79de
Samuel Granja <loboabel.cronox@gmail.com>
2021-10-08 15:16:45 -0500

    agregue al codigo la funcion randomNumbers la cual se usara para aparecer en el canvas a los pokemones aleatoriamiente, tambien agregue el objeto literal teclas con la que vamos a manejar el evento para mover un pokemon

---
+++ script/clas.js
@@ -0,0 +1,16 @@
+
+//funcion para numeros aleatorios
+function randomNumbers (min,max)
+{
+    var one= 1
+    var resultado;
+    resultado= Math.floor (Math.random () * (max - min + one)) + min
+}
+
+//objetos literales
+    var teclas = {
+        UP:37,
+        DAWN:38,
+        LEFT:39,
+        RIGHT:40
+    }
